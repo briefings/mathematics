4ac947ba0b63880574cf72a761ed2f98f503f6e8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-09 21:31:36 +0100

    Update src/main/scala/com/grey/data/DataRead.scala

--- src/main/scala/com/grey/data/DataRead.scala
+++ src/main/scala/com/grey/data/DataRead.scala
@@ -2,11 +2,11 @@ package com.grey.data
 
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.types.StructType
-import com.grey.data.DataCaseClass.Stocks
+
 
 class DataRead(spark: SparkSession) {
 
-  def dataRead(dataString: String, schema: StructType): Dataset[Stocks] = {
+  def dataRead(dataString: String, schema: StructType): DataFrame = {
 
     // implicits
     import spark.implicits._
@@ -18,8 +18,7 @@ class DataRead(spark: SparkSession) {
       .option("encoding", "UTF-8")
       .load(dataString)
 
-    // as a dataset
-    readings.as[Stocks]
+    readings
 
   }
 
