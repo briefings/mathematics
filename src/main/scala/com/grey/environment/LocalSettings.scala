f6a2b906462cf1b6d86d90c4aac58514182f0b1b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-09 19:54:40 +0100

    Add src/main/scala/com/grey/environment/LocalSettings.scala

---
+++ src/main/scala/com/grey/environment/LocalSettings.scala
@@ -0,0 +1,12 @@
+package com.grey.environment
+
+class LocalSettings {
+
+  // The environment
+  val projectDirectory: String = System.getProperty("user.dir")
+  val separator: String = System.getProperty("file.separator")
+
+  // Data directory
+  val dataDirectory = s"$projectDirectory$separator"
+
+}
