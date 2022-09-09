61a647ee9347e47c5054d3596f0e0d4015790236
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-09 21:15:33 +0100

    Update src/main/scala/com/grey/data/DataRead.scala

--- src/main/scala/com/grey/data/DataRead.scala
+++ src/main/scala/com/grey/data/DataRead.scala
@@ -1,5 +1,26 @@
 package com.grey.data
 
-class DataRead {
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.types.StructType
+import com.grey.data.DataCaseClass.Stocks
+
+class DataRead(spark: SparkSession) {
+
+  def dataRead(dataString: String, schema: StructType): Dataset[Stocks] = {
+
+    // implicits
+    import spark.implicits._
+
+    // the data
+    val readings: DataFrame = spark.read.schema(schema).format("csv")
+      .option("header", value = true)
+      .option("dateFormat", "yyyy-MM-dd")
+      .option("encoding", "UTF-8")
+      .load(dataString)
+
+    // as a dataset
+    readings.as[Stocks]
+
+  }
 
 }
