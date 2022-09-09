61516bb074e3a3ad24dd6e36a5e63206fb91d810
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-09 20:51:01 +0100

    Update src/main/scala/com/grey/data/DataSchema.scala

--- src/main/scala/com/grey/data/DataSchema.scala
+++ src/main/scala/com/grey/data/DataSchema.scala
@@ -1,12 +1,43 @@
 package com.grey.data
 
-class DataSchema {
+import com.grey.environment.LocalSettings
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DataType, StructType}
 
-  def dataSchema(): Unit = {
+import java.nio.file.Paths
+import scala.util.Try
+import scala.util.control.Exception
 
+/**
+ *
+ * @param spark: A SparkSession instance
+ */
+class DataSchema(spark: SparkSession) {
 
+  private val localSettings = new LocalSettings()
 
+  /**
+   *
+   * @param dataString: A data file's path & file string w.r.t. this project's <data> directory
+   */
+  def dataSchema(dataString: String): Try[StructType] = {
 
+    // reading-in a data schema
+    val fieldProperties: Try[RDD[String]] = Exception.allCatch.withTry(
+      spark.sparkContext.textFile(Paths.get(localSettings.dataDirectory, dataString).toString)
+    )
+
+    // convert the schema to a StructType
+    val schema: Try[StructType] = if (fieldProperties.isSuccess) {
+      Exception.allCatch.withTry(
+        DataType.fromJson(fieldProperties.get.collect.mkString("")).asInstanceOf[StructType]
+      )
+    } else {
+      sys.error(fieldProperties.failed.get.getMessage)
+    }
+
+    schema
 
   }
 
