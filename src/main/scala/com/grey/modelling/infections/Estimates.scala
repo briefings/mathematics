c3878ca838ad7ee2c2f92e9f94a469e2364a48f2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-10 21:24:00 +0100

    Update src/main/scala/com/grey/modelling/infections/Estimates.scala

--- src/main/scala/com/grey/modelling/infections/Estimates.scala
+++ src/main/scala/com/grey/modelling/infections/Estimates.scala
@@ -1,5 +1,21 @@
 package com.grey.modelling.infections
 
-class Estimates {
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+class Estimates(spark: SparkSession) {
+
+  def estimates(infections: Dataset[Row]): Unit = {
+
+    /**
+     * Import implicits for
+     *    encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+     *    implicit conversions, e.g., converting a RDD to a DataFrames.
+     *    access to the "$" notation.
+     */
+    import spark.implicits._
+
+    infections.select($"admission_date", $"age", $"asthma").show()
+
+  }
 
 }
