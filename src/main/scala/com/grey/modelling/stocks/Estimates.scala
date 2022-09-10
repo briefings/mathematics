dba9e4094611750b396367fe367b62c372e706af
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-10 21:29:45 +0100

    Update src/main/scala/com/grey/modelling/stocks/Estimates.scala

--- src/main/scala/com/grey/modelling/stocks/Estimates.scala
+++ src/main/scala/com/grey/modelling/stocks/Estimates.scala
@@ -2,21 +2,26 @@ package com.grey.modelling.stocks
 
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
+
 /**
  *
- * @param spark: A spark instance
+ * @param spark : A spark instance
  */
 class Estimates(spark: SparkSession) {
 
   private val dependenceMatrix = new DependenceMatrix()
 
+  /**
+   *
+   * @param stocks : The trading stocks data
+   */
   def estimates(stocks: Dataset[Row]): Unit = {
 
     /**
      * Import implicits for
-     *    encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
-     *    implicit conversions, e.g., converting a RDD to a DataFrames.
-     *    access to the "$" notation.
+     * encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+     * implicit conversions, e.g., converting a RDD to a DataFrames.
+     * access to the "$" notation.
      */
     import spark.implicits._
 
@@ -25,7 +30,6 @@ class Estimates(spark: SparkSession) {
 
     // The degree of correlation between the 4 stock level variables
     dependenceMatrix.dependenceMatrix(stocks = stocks)
-
 
   }
 
