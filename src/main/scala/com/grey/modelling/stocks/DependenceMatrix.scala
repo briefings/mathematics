5350d9809058213d2ad6e80ca12ee6188a8af0b2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-10 21:11:58 +0100

    Add src/main/scala/com/grey/modelling/stocks/DependenceMatrix.scala

---
+++ src/main/scala/com/grey/modelling/stocks/DependenceMatrix.scala
@@ -0,0 +1,26 @@
+package com.grey.modelling.stocks
+
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.stat.Correlation
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
+
+class DependenceMatrix() {
+
+  def dependenceMatrix(stocks: Dataset[Row]): Unit = {
+
+    // Hence
+    val assembler: VectorAssembler = new VectorAssembler()
+      .setInputCols(Array("open", "close", "high", "low"))
+      .setOutputCol("trade")
+
+    // Collinearity
+    val pearson: DataFrame = Correlation.corr(assembler.transform(stocks), column = "trade", method = "pearson")
+    pearson.show()
+
+    val spearman = Correlation.corr(assembler.transform(stocks), column = "trade", method = "spearman")
+    spearman.show()
+
+
+  }
+
+}
