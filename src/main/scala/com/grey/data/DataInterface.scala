686d643a56d3edf90423513fe4ed1823050c7d73
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-10 02:30:03 +0100

    Update src/main/scala/com/grey/data/DataInterface.scala

--- src/main/scala/com/grey/data/DataInterface.scala
+++ src/main/scala/com/grey/data/DataInterface.scala
@@ -1,11 +1,22 @@
 package com.grey.data
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.StructType
 
-class DataInterface(spark:SparkSession) {
+class DataInterface(spark: SparkSession) {
 
-  def dataInterface(): Unit = {
+  def dataInterface(dataString: String, schemaString: String): Dataset[Row] = {
 
+    // Read-in the data schema
+    val schema: StructType = new DataSchema(spark = spark).dataSchema(schemaString = schemaString)
+
+    // Hence, read-in the corresponding data set
+    val readings: DataFrame = new DataRead(spark = spark).dataRead(dataString = dataString, schema = schema)
+
+    // Converting to a spark data set
+    val frame: String = ScalaCaseClass.scalaCaseClass(schema = schema)
+    readings.as(frame)
 
   }
 
