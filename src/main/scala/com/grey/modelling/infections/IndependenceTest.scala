f9cde35420b26d90fa44c68604f9096e2a7b741a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-11 17:09:50 +0100

    Update src/main/scala/com/grey/modelling/infections/IndependenceTest.scala

--- src/main/scala/com/grey/modelling/infections/IndependenceTest.scala
+++ src/main/scala/com/grey/modelling/infections/IndependenceTest.scala
@@ -1,5 +1,24 @@
 package com.grey.modelling.infections
 
-class IndependenceTest {
+import org.apache.spark.ml.stat.ChiSquareTest
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+class IndependenceTest(spark: SparkSession) {
+
+  def independenceTest(data: Dataset[Row]): Unit = {
+
+    /**
+     * Import spark.implicits._ for
+     * encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+     * implicit conversions, e.g., converting a RDD to a DataFrames.
+     * access to the "$" notation.
+     */
+
+    val chi: DataFrame = ChiSquareTest.test(data, featuresCol = "features", labelCol = "label")
+
+    chi.show()
+
+
+  }
 
 }
