422c55bb91e38aee9b9482ee59aa6f018c5663ef
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-11 18:47:12 +0100

    Update src/main/scala/com/grey/modelling/infections/IndependenceTest.scala

--- src/main/scala/com/grey/modelling/infections/IndependenceTest.scala
+++ src/main/scala/com/grey/modelling/infections/IndependenceTest.scala
@@ -1,12 +1,23 @@
 package com.grey.modelling.infections
 
+import com.grey.functions.LabellingPoints
 import org.apache.spark.ml.stat.ChiSquareTest
 import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
+
+/**
+ *
+ * @param spark: A SparkSession instance
+ */
 class IndependenceTest(spark: SparkSession) {
 
-  def independenceTest(data: Dataset[Row]): Unit = {
+  /**
+   *
+   * @param dataIndexed: A data set wherein the factor variables have been indexed
+   * @param independentFactors: The array of independent factors
+   */
+  def independenceTest(dataIndexed: Dataset[Row], independentFactors: Array[String], dependentFactor: String): Unit = {
 
     /**
      * Import spark.implicits._ for
@@ -15,15 +26,24 @@ class IndependenceTest(spark: SparkSession) {
      * access to the "$" notation.
      */
 
-    val chi: DataFrame = ChiSquareTest.test(data, featuresCol = "features", labelCol = "label")
 
+    // Prior to determining the degree of dependence between each <independent factor variable>
+    // and the <dependent factor variable>, the data must be converted to a labelled point
+    // structure.
+    val dataLabelled: Dataset[Row] =  new LabellingPoints(spark = spark)
+      .labellingPoints(data = dataIndexed, independent = independentFactors, dependent = dependentFactor)
+
+
+    // Hence
+    val chi: DataFrame = ChiSquareTest.test(dataLabelled, featuresCol = "features", labelCol = "label")
     chi.printSchema()
 
+
+    // The estimates
+    println(independentFactors.mkString(", "))
     println("probabilities: " + chi.head.getAs[Vector[Double]](fieldName = "pValues"))
     println("degrees of freedom: " +  chi.head.getAs[Vector[IntegerType]](fieldName = "degreesOfFreedom"))
     println("statistics: " + chi.head.getAs[Vector[Double]](fieldName = "statistics"))
-
-
 
 
   }
