85f5ec23c78455f01012643eb8787d06e1942e50
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-11 18:47:24 +0100

    Update src/main/scala/com/grey/modelling/infections/Estimates.scala

--- src/main/scala/com/grey/modelling/infections/Estimates.scala
+++ src/main/scala/com/grey/modelling/infections/Estimates.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 /**
  *
- * @param spark : A SparkSession instance
+ * @param spark: A SparkSession instance
  */
 class Estimates(spark: SparkSession) {
 
@@ -27,17 +27,18 @@ class Estimates(spark: SparkSession) {
      */
 
 
-    // The outcome
+    // The dependent/outcome variable
     val label: String = "outcome"
-
-
-    // Factor variables
-    val factors = List("age", "sex", "asthma", "liver_mild", "renal", "pulmonary", "neurological",
-      "liver_mod_severe", "malignant_neoplasm", label)
+    
+    
+    // Factors
+    val independentFactors: Array[String] = Array("age", "sex", "asthma", "liver_mild", "renal", "pulmonary", "neurological",
+      "liver_mod_severe", "malignant_neoplasm")
+    val factors: Array[String] = independentFactors ++ Array(label)
 
 
     // Extraneous variables
-    val exclude = List("outcome_date", "admission_date")
+    val exclude: Array[String] = Array("outcome_date", "admission_date")
 
 
     // Add extra features
@@ -50,23 +51,16 @@ class Estimates(spark: SparkSession) {
     indexed.printSchema()
 
 
-    // Hence, the modelling variables; note that the indexed dependent variable is named <label>.
+    // Hence, the modelling variables
     val arguments: DataFrame = indexed.drop(factors: _*).drop(exclude: _*)
-      .withColumnRenamed(existingName = s"${label}_index", newName = "label")
-    val variablesIndexed: Dataset[Row] = arguments.as(
+    val dataIndexed: Dataset[Row] = arguments.as(
       ScalaCaseClass.scalaCaseClass(schema = arguments.schema))
+    dataIndexed.printSchema()
 
 
-    // The names of the independent variables
-    val independent: Array[String] = variablesIndexed.columns.filterNot(_ == s"${label}_index")
-
-
-   // Hence
-   val variablesLabelled: Dataset[Row] =  new LabellingPoints(spark = spark)
-     .labellingPoints(data = variablesIndexed, independent = independent)
-
-
-    independenceTest.independenceTest(data = variablesLabelled)
+    // Independence
+    independenceTest.independenceTest(dataIndexed = dataIndexed,
+      independentFactors = independentFactors, dependentFactor = label)
 
   }
 
