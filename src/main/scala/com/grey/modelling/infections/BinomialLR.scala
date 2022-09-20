1b3852a73b8f15bb43b79a7084ab71e3bff5efdb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-20 19:27:04 +0100

    Update src/main/scala/com/grey/modelling/infections/BinomialLR.scala

--- src/main/scala/com/grey/modelling/infections/BinomialLR.scala
+++ src/main/scala/com/grey/modelling/infections/BinomialLR.scala
@@ -1,24 +1,17 @@
 package com.grey.modelling.infections
 
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import com.grey.functions.LabellingPoints
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 class BinomialLR(spark: SparkSession) {
 
-  def binomialLR(dataEncoded: Dataset[Row], independentFactors: Array[String]): Unit = {
+  def binomialLR(dataEncoded: Dataset[Row], independentFactors: Array[String], dependentFactor: String): Unit = {
 
     val Array(training, testing) = dataEncoded.randomSplit(Array(0.8, 0.2), seed = 5)
     training.show(5)
     testing.show(5)
 
-    val vectorAssembler: VectorAssembler = new VectorAssembler()
-      .setInputCols(independentFactors)
-      .setOutputCol("label")
-
-    val trainingData: DataFrame = vectorAssembler.transform(training)
-    val testingData: DataFrame = vectorAssembler.transform(testing)
-    trainingData.show()
-    testingData.show()
+    
 
   }
 
